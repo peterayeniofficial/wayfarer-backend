4eace397271cc104a96938e97a218ab3eafd0d8d
Peter Ayeni <peterayeni@Peters-MacBook-Pro.local>
2019-07-08 23:07:26 +0100

    chore(setup): Add Read me and folder structures

---
+++ README.md
@@ -0,0 +1,3 @@
+# Wayfarer Backend Service
+
+WayFarer is a public bus transportation booking server. You are required to develop the back-end API.
--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
+import logger from 'morgan'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
 const port = process.env.PORT || 9001
+
+app.use(logger('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors('*'))
 
 
 // Serve when a random route is inputed
