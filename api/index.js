79d86da265c1f125ccd2f878235b0f17255e22a3
Peter Ayeni <peterayeni@Peters-MacBook-Pro.local>
2019-07-09 02:04:33 +0100

    fix error in codeclimate

--- api/index.js
+++ api/index.js
@@ -1,25 +1,26 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import logger from 'morgan'
-import dotenv from 'dotenv'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import logger from 'morgan';
+import dotenv from 'dotenv';
 
-dotenv.config()
-const app = express()
-const port = process.env.PORT || 9001
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 9001;
 
-app.use(logger('dev'))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cors('*'))
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors('*'));
 
 
 // Serve when a random route is inputed
 app.get('*', (req, res) => res.status(200).send({
-    message: 'Welcome to Wayfarer Service'
-}))
+    message: 'Welcome to Wayfarer Service',
+}));
 
 app.listen(port, () => {
-    console.log(`Service started on PORT ${port}`)
-})
+    // eslint-disable-next-line no-console
+    console.log(`Service started on PORT ${port}`);
+});
 
-export default app
+export default app;
